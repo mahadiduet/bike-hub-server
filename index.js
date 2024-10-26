1e6a19ff3e01893cb7b727220ccd4eb0952434db
mahadiduet <mahadiduet@gmail.com>
2024-10-27 00:05:46 +0600

    Dot env file update

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const { ObjectId } = require('mongodb');
+require('dotenv').config();
 
 
 const app = express()
